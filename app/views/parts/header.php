ee95030a09b640792245fb5977344e6ba1f52f9b
NEO7171 <dgedai2007@yandex.ru>
2022-05-16 15:52:02 +0300

    Урок 12.Виджет меню категорий

--- app/views/parts/header.php
+++ app/views/parts/header.php
@@ -81,7 +81,11 @@ use \wfm\View;
                     </button>
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <?php new \app\widgets\menu\Menu([
+                            'class' => 'navbar-nav ms-auto mb-2 mb-lg-0',
+                            'cache' => 30,
+                        ]) ?>
+                        <!--<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link" href="category.html">Компьютеры</a>
                             </li>
@@ -89,8 +93,7 @@ use \wfm\View;
                                 <a class="nav-link" href="category.html">Планшеты</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                   data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Ноутбуки
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -104,7 +107,7 @@ use \wfm\View;
                             <li class="nav-item">
                                 <a class="nav-link" href="category.html">Камеры</a>
                             </li>
-                        </ul>
+                        </ul>-->
                     </div>
 
                 </div>
